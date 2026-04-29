4e131a0e0e47ad938f892cdc6f8ef34d8dd06407
hza81007155 <800755@qq.com>
2026-04-29 10:58:20 +0800

    Update diy-part2-18.06.sh

--- diy-part2-18.06.sh
+++ diy-part2-18.06.sh
@@ -69,8 +69,8 @@ git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-
 git clone --depth=1 -b main https://github.com/Openwrt-Passwall/openwrt-passwall2 package/openwrt-passwall2
 
 # Argone theme
-git clone --depth=1 -b master https://github.com/hza81007155/luci-theme-argone package/luci-theme-argon
-git clone --depth=1 -b master https://github.com/hza81007155/luci-app-argone-config.git package/luci-app-argon-config
+git clone --depth=1 -b main https://github.com/hza81007155/luci-theme-argone package/luci-theme-argon
+git clone --depth=1 -b main https://github.com/hza81007155/luci-app-argone-config.git package/luci-app-argon-config
 
 # istore
 git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci package/nas-packages-luci
