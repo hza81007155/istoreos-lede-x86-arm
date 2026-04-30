fdeb6bf44299eb8cc457b5da310f4b4e8ab3ae25
hza81007155 <800755@qq.com>
2026-04-30 10:14:26 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -28,8 +28,6 @@ rm -rf feeds/packages/net/xray-core
 rm -rf feeds/packages/net/lucky
 rm -rf feeds/packages/utils/coremark
 rm -rf feeds/packages/net/ksmbd
-./scripts/feeds uninstall ksmbd
-./scripts/feeds install ksmbd
 
 # x86 型号只显示 CPU 型号
 sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
@@ -51,24 +49,11 @@ function git_sparse_clone() {
 
 # 添加插件
 git_sparse_clone openwrt-24.10 https://github.com/openwrt/packages utils/coremark
-git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash package/luci-app-openclash
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 git clone --depth=1 -b main https://github.com/Openwrt-Passwall/openwrt-passwall package/luci-app-passwall
 git clone --depth=1 -b main https://github.com/Openwrt-Passwall/openwrt-passwall2 package/openwrt-passwall2
-git clone https://github.com/sirpdboy/luci-app-taskplan package/luci-app-taskplan
-git clone https://github.com/miaoermua/luci-app-leigod-acc package/luci-app-leigod-acc
-
-# adguardHome
-git clone --depth=1 -b master https://github.com/rufengsuixing/luci-app-adguardhome.git
-
-# cpufreq
-git clone --depth=1 --single-branch https://github.com/hza81007155/luci-app-cpufreq.git
 
 # istore
-#git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci package/nas-packages-luci
-#git clone --depth=1 -b master https://github.com/linkease/nas-packages package/nas-packages
-#git clone --depth=1 -b main https://github.com/linkease/istore package/istore
 git clone https://github.com/linkease/istore-ui.git package/istore/istore-ui
 git clone https://github.com/linkease/istore.git package/istore/istore
 git clone https://github.com/linkease/nas-packages.git package/nas
@@ -78,40 +63,5 @@ git clone https://github.com/linkease/nas-packages-luci.git package/luci-app-nas
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 
 # Argone theme
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
-
-# openclash
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash package/luci-app-openclash
-
-#预置OpenClash内核和GEO数据
-export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
-export CORE_TUN=https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux
-export CORE_DEV=https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux
-export CORE_MATE=https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux
-
-export CORE_TYPE=$(echo $OWRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
-export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
-
-export GEO_MMDB=https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb
-export GEO_SITE=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat
-export GEO_IP=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat
-
-cd ./OpenClash/luci-app-openclash/root/etc/openclash
-
-curl -sfL -o ./Country.mmdb $GEO_MMDB
-curl -sfL -o ./GeoSite.dat $GEO_SITE
-curl -sfL -o ./GeoIP.dat $GEO_IP
-
-mkdir ./core && cd ./core
-
-curl -sfL -o ./tun.gz "$CORE_TUN"-"$CORE_TYPE"-"$TUN_VER".gz
-gzip -d ./tun.gz && mv ./tun ./clash_tun
-
-curl -sfL -o ./meta.tar.gz "$CORE_MATE"-"$CORE_TYPE".tar.gz
-tar -zxf ./meta.tar.gz && mv ./clash ./clash_meta
-
-curl -sfL -o ./dev.tar.gz "$CORE_DEV"-"$CORE_TYPE".tar.gz
-tar -zxf ./dev.tar.gz
-
-chmod +x ./clash* ; rm -rf ./*.gz
+git clone -b 18.06 https://github.com/hza81007155/luci-theme-argon.git luci-theme-argon
+git clone -b 18.06 https://github.com/hza81007155/luci-app-argon-config.git luci-app-argon-config
